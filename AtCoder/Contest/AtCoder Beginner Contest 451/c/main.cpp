57a503a030991d7cb4c5a14f526573b83d5a1bfb
UB <lib5542@naver.com>
2026-07-29 10:49:19 +0900

    ABC 451 solve

---
+++ AtCoder/Contest/AtCoder Beginner Contest 451/c/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+
+    vector<ll> dp(30001, 0);
+
+    // TLE가 남. 기본적으로 Q 범위가 3*10000인데 쿼리를 Q만큼 처리하므로 O(n). 모든 연산이 log(n)이어야 함. 이걸 어떻게 할 지 모르겠음
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    /*
+    소수 출력 사용 시
+    cout << fixed;
+    cout.precision(10);
+    */
+
+    solve();
+
+    return 0;
+}
