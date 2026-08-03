e1c6f0948d64378c42953ff769e04d36bf088d18
UB <lib5542@naver.com>
2026-08-03 13:49:16 +0900

    priority queue practice

--- Practice/priority_queue/main.cpp
+++ Practice/priority_queue/main.cpp
@@ -31,11 +31,11 @@ int main() {
     };
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(myCmp)> pq3(myCmp);
-    pq3.emplace(pair<int, int>(20, 40));
-    pq3.emplace(pair<int, int>(10, 20));
-    pq3.emplace(pair<int, int>(50, 400));
-    pq3.emplace(pair<int, int>(90, 60));
-    pq3.emplace(pair<int, int>(60, 10));
+    pq3.emplace(20, 40);
+    pq3.emplace(10, 20);
+    pq3.emplace(50, 400);
+    pq3.emplace(90, 60);
+    pq3.emplace(60, 10);
 
     while (!pq3.empty()) {
         cout << pq3.top().first << " " << pq3.top().second << "\n";
